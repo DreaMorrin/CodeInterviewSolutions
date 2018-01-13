6a6b6e4bf35cdae2e0967965fa75c7cd5d75e16a
Xidong Wang <nash.xidong.wang@gmail.com>
2018-01-13 15:54:11 -0700

    BinarySearchTreeSuccessor: update it with Root node and Target Node as input parameters

--- src/BinarySearchTreeSuccessor.java
+++ src/BinarySearchTreeSuccessor.java
@@ -5,29 +5,49 @@ Given the root of a binary search tree and a value, find the "successor" of that
 public class BinarySearchTreeSuccessor {
 	
 	class Node {
-		Node p;
 		Node left;
 		Node right;
 		int val;
 	}
 	
-	static Node successor(Node n) {
+	static Node successor(Node root, Node target) {
 		
-		// look for it, if right sub tree is not empty
-		if (n.right != null) {
-			n = n.right;
+		Node parent = null;
+		while (root != null) {
 			
-			Node successor = n;
-			// get left most leaf node
-			while (n != null) {
-				successor = n;
-				n = n.left;
+			// find target node
+			if (target.val == root.val) {
+				
+				// look for it, if right sub tree is not empty
+				if (target.right != null) {
+					Node successor = target;
+					
+					// get left most leaf node
+					while (target != null) {
+						successor = target;
+						target = target.left;	
+					}
+					
+					return successor;
+				}
+				// if there is no right subtree for target node, the successor is parent node
+				else {
+					return parent;
+				}
 			}
 			
-			return successor;
+			// binary search
+			else if (target.val < root.val) {
+				parent = root;
+				root = root.left;
+			}
+			else if (target.val > root.val) {
+				parent = root;
+				root = root.right;
+			}
 		}
 		
-		return n.p;
+		return null;
 	}
 
 	public static void main(String[] args) {
